ae1ec2ffedfda40096367fbf5ed789762427640b
Alexandre Bezroutchko <abb@gremwell.com>
2012-06-02 05:12:38 +0200

    make it easier to kill this script

--- pox/mitmer/test.sh
+++ pox/mitmer/test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -x
 
 sudo ovs-vsctl del-br mitm0
-script -c 'while true ; do python pox.py --no-cli mitmer nm.test ; done'
+script -c 'while sleep 1 ; do python pox.py --no-cli mitmer nm.test ; done'
